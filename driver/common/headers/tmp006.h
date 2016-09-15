c70ce0bc981225233255497f00eabad59e301d78
Ahmad Enany <ahmad.enany@anaren.com>
2016-09-15 15:26:07 -0400

    Fixed minor spelling mistake

--- driver/common/headers/tmp006.h
+++ driver/common/headers/tmp006.h
@@ -199,7 +199,7 @@ bool TMP006_GetDataReadyStatus(uint8_t id);
 /**
 Read the ambient (die) temperature.  When set to Continuous Conversion mode, the
 device periodically performs temperature conversions at a predefined rate.  This
-function calulates the temperature using only the most recent conversion value.
+function calculates the temperature using only the most recent conversion value.
 @param id device ID (0 to 7) on i2c bus
 @return temperature of the TMP006 die in Celsius
 */
